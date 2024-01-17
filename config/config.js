2b9592f89c2414c23cbc226ff4af06060e9b18f4
Trungdeptrai <chundeptraii@gmail.com>
2024-01-17 16:01:12 +0700

    bai59

--- config/config.js
+++ config/config.js
@@ -23,10 +23,17 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.DB_DIALECT || 'postgres',
+    port: process.env.DB_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true
+      }
+    },
+    dialectModule: pg
   },
 };
